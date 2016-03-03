1ab149b69d6cee1abcdd5a5fb68fc98576546ee2
Danilo Horta <danilo.horta@gmail.com>
2016-03-03 15:04:54 +0000

    0.0.3

--- limix_misc/inspect_.py
+++ limix_misc/inspect_.py
@@ -3,13 +3,17 @@ import imp
 import inspect
 import re
 
-def _fetch_entity(script_filepath, regex, entity):
+def _fetch_entity(script_filepath, regex, entity, def_within=True):
     script_name = os.path.basename(script_filepath)
-    mod_name = os.path.splitext(script_name)[0]
+    # mod_name = os.path.splitext(script_name)[0]
+    mod_name = inspect.getmodulename(script_filepath)
     mod = imp.load_source(mod_name, script_filepath)
 
     funcs = []
     for func in inspect.getmembers(mod, entity):
+        if def_within:
+            if mod_name != func[1].__module__:
+                continue
         func_name = func[0]
         m = re.match(regex, func_name)
         if m:
