53cdf20dfb525488b9f9fd9dccc485fe4c5a4ad2
Danilo Horta <danilo.horta@gmail.com>
2016-03-07 18:34:12 +0000

    as float

--- limix_util/scalar.py
+++ limix_util/scalar.py
@@ -1,6 +1,13 @@
+import numpy as np
+
 def isfloat(value):
     try:
         float(value)
         return True
     except ValueError:
         return False
+
+def asfloat(value):
+    if isfloat(value):
+        return value
+    return np.asscalar(value, float)
