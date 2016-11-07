d3a3e2a08cc4b92bd8bd0456688f66e7443113b1
Danilo Horta <danilo.horta@gmail.com>
2016-11-07 17:26:24 +0000

    progressbar2

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ def setup_package():
     pytest_runner = ['pytest-runner'] if needs_pytest else []
 
     setup_requires = [] + pytest_runner
-    install_requires = ['pytest', 'scipy>=0.17', 'numpy>=1.9', 'humanfriendly',
-                        'asciitree']
+    install_requires = ['pytest', 'numpy>=1.9', 'humanfriendly',
+                        'asciitree', 'progressbar2']
     tests_require = install_requires
 
     metadata = dict(
